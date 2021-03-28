4979c1a04759f26a7fb1999e9a9616f78d02df94
JagodaCubrilo <jagoda.cubrilo@yahoo.com>
2021-03-28 16:40:56 +0200

    adding tests

--- app/lib.js
+++ app/lib.js
@@ -1,2 +1,8 @@
 // generating random number
 // string or not a string in palindrome
+// get a number
+
+getNumber = function () {
+  return 111;
+};
+module.exports = { getNumber };
---
+++ test/index.js
@@ -0,0 +1,124 @@
+// test-runner
+
+//dependecies
+const lib = require('./app/lib.js');
+const assert = require('assert');
+
+//app logic for test runner
+_app = {};
+
+//container for the test
+_app.tests = {
+  'unit': {},
+};
+
+//assert get a number is returning a number
+_app.tests.unit['lib.getNumber should return number'] = function (done) {
+  //when test is done running it should return : done(convention)
+  const val = lib.getNumber();
+  assert.strictEqual(typeof val, 'number');
+  done();
+};
+
+//assert get a number is returning a number 111
+_app.tests.unit['lib.getNumber should return 111'] = function (done) {
+  //when test is done running it should return : done(convention)
+  const val = lib.getNumber();
+  assert.strictEqual(val, 111);
+  done();
+};
+
+//assert get a number is returning a number 2
+_app.tests.unit['lib.getNumber should return 111'] = function (done) {
+  //when test is done running it should return : done(convention)
+  const val = lib.getNumber();
+  assert.strictEqual(val, 2);
+  done();
+};
+
+// Count all the tests
+_app.countTests = function(){
+    let counter = 0;
+    for(const key in _app.tests){
+       if(_app.tests.hasOwnProperty(key)){
+         const subTests = _app.tests[key];
+         for(const testName in subTests){
+            if(subTests.hasOwnProperty(testName)){
+              counter++;
+            }
+         }
+       }
+    }
+    return counter;
+  };
+
+//run all the tests, collecting the errors and successes
+_app.runTest() = function () {
+  const errors = [];
+  const successes = 0;
+  const limit = _app.countTests();
+  const counter = 0;
+  for (const key in _app.tests) {
+    if (_app.tests.hashOwnProperty(key)) {
+      const subTests = _app.tests[key];
+      for (const testName in subTests) {
+        if (subTests.hashOwnProperty(testName)) {
+          (function () {
+            const tempTestName = testName;
+            const testValue = subTests[testName];
+            // call the test
+
+            try {
+              testValue(function () {
+                //if it calls back : without throwing : it succeeded
+                console.log('\x1b[32m%s\x1b[0m',tmpTestName);
+                counter++;
+                successes++;
+                if(counter == limit){
+                  _app.produceTestReport(limit,successes,errors);
+                }
+              });
+            } catch (e) {
+                errors.push({
+                    'name' : testName,
+                    'error' : e
+          });
+          console.log('\x1b[31m%s\x1b[0m',tmpTestName);
+                counter++;
+                if(counter == limit){
+                    _app.produceTestReport(limit,successes,errors);
+                  }
+        }
+      })();
+    }
+  }
+};
+
+// Product a test outcome report
+_app.produceTestReport = function(limit,successes,errors){
+    console.log("");
+    console.log("--------BEGIN TEST REPORT--------");
+    console.log("");
+    console.log("Total Tests: ",limit);
+    console.log("Pass: ",successes);
+    console.log("Fail: ",errors.length);
+    console.log("");
+  
+    // If there are errors, print them in detail
+    if(errors.length > 0){
+      console.log("--------BEGIN ERROR DETAILS--------");
+      console.log("");
+      errors.forEach(function(testError){
+        console.log('\x1b[31m%s\x1b[0m',testError.name);
+        console.log(testError.error);
+        console.log("");
+      });
+      console.log("");
+      console.log("--------END ERROR DETAILS--------");
+    }
+  
+  
+    console.log("");
+    console.log("--------END TEST REPORT--------");
+  
+  };
