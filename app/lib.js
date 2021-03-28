5bff29bdf9290c61afc981e7d0cf75c45704afb9
JagodaCubrilo <jagoda.cubrilo@yahoo.com>
2021-03-28 15:20:56 +0200

    starting with assigement

---
+++ app/lib.js
@@ -0,0 +1,2 @@
+// generating random number
+// string or not a string in palindrome
---
+++ test/test-runner.js
@@ -0,0 +1,3 @@
+// tests for functions in lib
+// test that they return a callback function
+//correct value when passed valid parameters, and that they return a predictable value (and don't crash) when passed invalid parameters.
